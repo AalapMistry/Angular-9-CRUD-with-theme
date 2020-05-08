31715947e4b9d3f8c58cb587c60d0568e0c5086e
AalapMistry <mistryaalap@gmail.com>
2020-05-08 17:13:35 +0530

    removed un use files

--- src/app/product/product.class.ts
+++
@@ -1,9 +0,0 @@
-export class ProductModel {
-    id: number;
-    ProductName: string;
-    Description: string;
-    Categories: string;
-    Stock:string;
-    Price:number;
-    Image: string;
-}
